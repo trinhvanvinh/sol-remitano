7dc097305183a62ddf6fab0978533e262821b39d
vinhtrinhvan <trinhvanvinh018>
2023-06-21 16:33:17 +0700

    init project

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    msg!("hello world");
+
+    let account_iter = &mut accounts.iter();
+    let account = next_account_info(account_iter)?;
+    if account.owner != program_id {
+        msg!("Greeterd account does not have correct");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
